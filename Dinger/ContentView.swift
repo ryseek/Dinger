d812e821d5f380a5c00b548f32ecbfc5b7dba748
Ruslan Suleimanov <ryseek1@gmail.com>
2026-04-20 19:24:17 +0200

    Initial Commit

---
+++ Dinger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dinger
+//
+//  Created by RUSLAN SULEIMANOV on 20.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dinger/DingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DingerApp.swift
+//  Dinger
+//
+//  Created by RUSLAN SULEIMANOV on 20.04.26.
+//
+
+import SwiftUI
+
+@main
+struct DingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
